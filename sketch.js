72a35bf3d262dfb77870b5a508685ef4884fcf54
Toni Anguiano <toni.anguiano@sjsu.edu>
2020-10-30 01:54:23 -0700

    background

--- sketch.js
+++ sketch.js
@@ -1,55 +1,31 @@
+var bgImg;
+var x1 = 0;
+var x2;
 
+var scrollSpeed = 2;
+
+function preload(){
+	bgImg = loadImage("images/pexels-felix-mittermeier-1205301.jpg");
+}
 
 function setup() {
-  createCanvas(1590, 700, WEBGL);
+  createCanvas(2000, 900);
 
+  x2 = width;
 }
 
 function draw() {
-  background(0);
+	image(bgImg, x1, 0, width, height);
+  image(bgImg, x2, 0, width, height);
 
-  let radius = width * 1.5;
+  x1 -= scrollSpeed;
+  x2 -= scrollSpeed;
 
-  //drag to move the world.
-  orbitControl();
-
-  normalMaterial();
-  translate(0, 0, -600);
-  for (let i = 0; i <= 12; i++) {
-    for (let j = 0; j <= 12; j++) {
-      push();
-      let a = (j / 12) * PI;
-      let b = (i / 12) * PI;
-      translate(
-        sin(2 * a) * radius * sin(b),
-        (cos(b) * radius) / 2,
-        cos(2 * a) * radius * sin(b)
-      );
-      if (j % 2 === 0) {
-        sphere(50, 50, 50);
-      } else {
-        sphere(30, 30, 30);
-      }
-      pop();
-    }
+  if (x1 < -width){
+    x1 = width;
+  }
+  if (x2 < -width){
+    x2 = width;
   }
 
-  let locX = mouseX - height / 2;
-  let locY = mouseY - width / 2;
-
-  ambientLight(255);
-  directionalLight(255, 25, 0, 0.25, 0.25, 0);
-  pointLight(0, 0, 255, locX, locY, 255);
-
-  push();
-  translate(-width / 4, 0, 0);
-  rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.02);
-  specularMaterial(250);
-  box(100, 100, 100);
-  pop();
-
-  translate(width / 4, 0, 0);
-  ambientMaterial(250);
-  box(100, 100, 100);
 }
